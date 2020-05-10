82920ed0daa277979090a2550d6cebcb200da163
morningjoesoftware <65132179+morningjoesoftware@users.noreply.github.com>
2020-05-10 14:22:00 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mjbackup-morningjoesoftware", # Replace with your own username
+    version="1.0.0",
+    author="morningjoe",
+    author_email="morningjoe@morningjoesoftware.com",
+    description="Morning Joe Software Linux Server Backup System",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    data_files=[('configs', ['conf/backup1.xml', 'conf/backup2.xml', 'conf/backup3.xml', 'conf/mjbackupConfig.xml', 'conf/mjbackupMysql.conf', 'conf/mjbackupPgsql.conf', 'conf/mjsync.conf']),
+            ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX',
+        'Topic :: System :: Archiving :: Backup',
+        'Topic :: System :: Archiving :: Compression',
+    ],
+    platforms=['posix'],
+    python_requires='>=3.6',
+)
